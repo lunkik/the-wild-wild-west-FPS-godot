84f04c0df0e28c9c1130207d15bedc7f3690a34d
Lunkik <lanik.ledford@gmail.com>
2024-06-12 16:58:36 -0500

    fixed sprint and reduced walk speed

--- player.gd
+++ player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody3D
 
-var speed
-const SPRINT_SPEED = 15.0
-const WALK_SPEED = 8.5
+var speed = 1.0
+const SPRINT_SPEED = 8.5
+const WALK_SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-const sensitivity = 0.02
+const sensitivity = 0.01
 const BOB_freq = 2.0
 const BOB_amp = 0.08
 var t_BOB = 0
@@ -34,25 +34,28 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-		if Input.is_action_just_pressed("sprint"):
-			speed = SPRINT_SPEED
-		else:
-			speed = WALK_SPEED	
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
-		velocity.x = direction.x * WALK_SPEED
-		velocity.z = direction.z * WALK_SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
 		velocity.x = 0.0
-		velocity.z = move_toward(velocity.z, 0, WALK_SPEED)
+		velocity.z = move_toward(velocity.z, 0, speed)
+		
 		t_BOB += delta * velocity.length() * float(is_on_floor())
 		camera.transform.origin = _headbob(t_BOB)
 
 	move_and_slide()
+	
 func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
 	pos.y = sin(time * BOB_freq) * BOB_amp
