1c512fa77e0ecb0e7dd3ad0898068357537d88e7
Lunkik <lanik.ledford@gmail.com>
2024-06-12 15:55:25 -0500

    sprint added

--- player.gd
+++ player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+var speed
+const SPRINT_SPEED = 10.0
+const WALK_SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const sensitivity = 0.03
 const BOB_freq = 2.0
@@ -32,17 +33,22 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+		if Input.is_action_just_pressed("sprint"):
+			speed = SPRINT_SPEED
+		else:
+			speed = WALK_SPEED	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * WALK_SPEED
+		velocity.z = direction.z * WALK_SPEED
 	else:
 		velocity.x = 0.0
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, WALK_SPEED)
 		t_BOB += delta * velocity.length() * float(is_on_floor())
 		camera.transform.origin = _headbob(t_BOB)
 
