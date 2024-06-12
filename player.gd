30fd1cb382e49e25fa99b859037cb9d37f1e4c11
Lunkik <lanik.ledford@gmail.com>
2024-06-12 14:28:09 -0500

    added camera movement

--- player.gd
+++ player.gd
@@ -3,9 +3,22 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const sensitivity = 0.03
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = 9.8
+@onready var head = $head
+@onready var camera = $head/Camera3D
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func  _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * sensitivity)
+		camera.rotate_x(-event.relative.y * sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+	
+
 
 
 func _physics_process(delta):
@@ -14,7 +27,7 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -26,6 +39,6 @@ func _physics_process(delta):
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = 0.0
-		velocity.z = 0.0
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
